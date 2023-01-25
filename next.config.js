f28d775e00ae4333c1d0f2530e2de91277dbb3ff
ohprettyhak <haklee.googl@gmail.com>
2023-01-26 03:08:24 +0900

    feat: config api proxy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: true, async rewrites() {
+    return [{
+      source: '/api/:path*', destination: `https://api.hufsdevelopers.org/calguksu/:path*`,
+    }];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
