b50009685220ab55e165bb27d600930e5a42cd5a
ohprettyhak <haklee.googl@gmail.com>
2023-03-05 21:14:36 +0900

    fix: remove unused path

--- pages/calendar/[name]/subscribe.tsx
+++
@@ -1,8 +0,0 @@
-import Layout from '@/components/Layout';
-import Subscribe from '@/containers/Subscribe';
-
-export default function SubscribePage() {
-  <Layout>
-    <Subscribe />
-  </Layout>;
-}
