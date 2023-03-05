d0a30f64ebe6e868cdf6efe67a61e40cb263ec1c
ohprettyhak <haklee.googl@gmail.com>
2023-03-05 16:50:05 +0900

    fix: seo title data

--- seo-config.ts
+++ seo-config.ts
@@ -1,10 +1,10 @@
 export default {
-  titleTemplate: `%s | 칼국수`,
+  titleTemplate: `%s - 칼국수`,
   defaultTitle: `칼국수`,
   description: `칼국수의 캘린더 구독 서비스 - 매월 새로운 맛, 새로운 경험! 끓는 국물과 탱글한 면발, 그리고 풍부한 재료들이 가득한 칼국수의 구독 서비스를 만나보세요.`,
   url: `https://calguksu.com`,
   openGraph: {
-    title: `%s | 칼국수`,
+    title: `칼국수`,
     description:
       '칼국수의 캘린더 구독 서비스 - 매월 새로운 맛, 새로운 경험! 끓는 국물과 탱글한 면발, 그리고 풍부한 재료들이 가득한 칼국수의 구독 서비스를 만나보세요.',
     url: `https://calguksu.com`,
