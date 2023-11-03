c58a0860ea03ecccbe996413fc83a0e5d0efa250
ohprettyhak <haklee.googl@gmail.com>
2023-11-03 13:46:50 +0900

    feat: change theme mode logic and add theme provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import useScreenSize from '@/hooks/useScreenSize';
 import FullyFlow from '@/components/common/FullyFlow';
 import FullyFlowSection from '@/components/common/FullyFlow/FullyFlowSection';
 
-import IntroduceSection from '@/components/IntroduceSection';
+import IntroduceSection from '../components/IntroSection';
 import ScheduleSection from '@/components/ScheduleSection';
 import SubscribeSection from '@/components/SubscribeSection';
 import CompleteSection from '@/components/CompleteSection';
@@ -13,11 +13,12 @@ import CompleteSection from '@/components/CompleteSection';
 import { GradientOval } from '@/styles/ui.styles';
 import Header from '@/components/common/Header';
 import ToastComponent from '@/components/common/Toast/ToastComponent';
+import { ThemeProvider } from '@/components/common/Theme/ThemeProvider';
 
 export default function Home() {
   useScreenSize();
 
-  return (<>
+  return (<ThemeProvider>
       <Header />
       <GradientOval />
       <FullyFlow>
@@ -27,6 +28,6 @@ export default function Home() {
         <FullyFlowSection><CompleteSection /></FullyFlowSection>
       </FullyFlow>
       <ToastComponent />
-    </>
+    </ThemeProvider>
   );
 }
---
+++ src/components/common/Theme/ThemeProvider.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode, useEffect, useState } from 'react';
+import ThemeContext from '@/components/common/Theme/Theme.context';
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+const getInitialTheme = () => {
+  const storedTheme = localStorage.getItem('theme');
+  if (storedTheme) return storedTheme;
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+};
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  const [theme, setTheme] = useState('undefined');
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setTheme(getInitialTheme());
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
