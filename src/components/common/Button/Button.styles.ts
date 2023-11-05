36b39e4059d74d40e62ee68b9d135afaa64a21c2
ohprettyhak <haklee.googl@gmail.com>
2023-11-06 01:48:33 +0900

    feat(select): add button component hover and invalid events

--- src/components/common/Button/Button.styles.ts
+++ src/components/common/Button/Button.styles.ts
@@ -2,9 +2,8 @@ import styled from 'styled-components';
 
 export const ButtonBlock = styled.button<{ $width: string }>`
   align-items: center;
-  background-color: var(--calguksu-colors-blue-normal);
   border-radius: 7px;
-  color: var(--calguksu-colors-white);
+  color: ${props => (props['aria-invalid'] === true ? 'var(--calguksu-colors-gray-700)' : 'var(--calguksu-colors-white)')};
   display: flex;
   font-size: 15px;
   font-weight: 600;
@@ -14,9 +13,10 @@ export const ButtonBlock = styled.button<{ $width: string }>`
   padding: 12px 13px;
   transition: all 200ms ease;
   width: ${({ $width }) => $width};
+  background-color: ${props => (props['aria-invalid'] === true ? 'var(--calguksu-colors-blue-invalid)' : 'var(--calguksu-colors-blue-normal)')};
 
   &:hover {
-    background-color: var(--calguksu-colors-blue-hover);
-    color: var(--calguksu-colors-blue-hover-text);
+    background-color: ${props => (props['aria-invalid'] === true ? 'var(--calguksu-colors-blue-invalid)' : 'var(--calguksu-colors-blue-hover)')};
+    color: ${props => (props['aria-invalid'] === true ? 'var(--calguksu-colors-gray-700)' : 'var(--calguksu-colors-blue-hover-text)')};
   }
 `;
