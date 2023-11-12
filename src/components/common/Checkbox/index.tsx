86ec574e5f3a51b4730d1b2c7df8633156dca26a
ohprettyhak <haklee.googl@gmail.com>
2023-11-13 01:07:49 +0800

    feat(checkbox): add checkbox component

---
+++ src/components/common/Checkbox/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  CheckboxContainer, CheckboxLabel, HiddenCheckbox, StyledCheckbox,
+} from '@/components/common/Checkbox/Checkbox.styles';
+import CheckmarkIcon from '@/components/common/icon/checkmark-icon';
+
+interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+  checked: boolean;
+  onClick: () => void;
+  markColor: string;
+  label?: string;
+}
+
+const Checkbox: React.FC<CheckboxProps> = ({ className, checked, onClick, markColor, label, ...props }) => (
+  <CheckboxContainer className={className}>
+    <HiddenCheckbox checked={checked} onChange={() => undefined} {...props} />
+    <StyledCheckbox checked={checked} onClick={onClick}>
+      <CheckmarkIcon width={13} height={13} checked={checked} stroke={markColor} />
+    </StyledCheckbox>
+    {label && <CheckboxLabel onClick={onClick}>{label}</CheckboxLabel>}
+  </CheckboxContainer>
+);
+
+export default Checkbox;
